05c0236839a0db8cba07523ad4c610284dfcb250
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2023-06-26 13:01:55 +0300

    feat : adding directly params to link

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
+#!/usr/bin/env node
+
 const readline = require("readline");
+const { program } = require("commander");
 const getLyrics = require("./library/getLyrics");
 const getSong = require("./library/getSong");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+program
+  .command("lyrics <title> <artist>")
+  .description("Get lyrics for a song")
+  .action((title, artist) => {
+    const options = {
+      apiKey: "SZ2FBOqVDvzzk7OAVIzD06a9nDPlbEV6qsVIMzzqFUHS9klnUBzsK3dXUvoMTV2E",
+      title,
+      artist,
+      optimize: true,
+    };
 
-const options = {
-  apiKey: "SZ2FBOqVDvzzk7OAVIzD06a9nDPlbEV6qsVIMzzqFUHS9klnUBzsK3dXUvoMTV2E",
-  title: "",
-  artist: "",
-  optimize: true,
-};
-
-rl.question("Enter the title of the song: ", (title) => {
-  options.title = title;
-
-  rl.question("Enter the artist: ", (artist) => {
-    options.artist = artist;
-
-    rl.close();
-
-    // Fetch lyrics
     getLyrics(options).then((lyrics) => {
       console.log("Lyrics:");
       console.log(lyrics);
     });
+  });
 
-    // Fetch song
+program
+  .command("song <title> <artist>")
+  .description("Get a song")
+  .action((title, artist) => {
+    const options = {
+      apiKey: "SZ2FBOqVDvzzk7OAVIzD06a9nDPlbEV6qsVIMzzqFUHS9klnUBzsK3dXUvoMTV2E",
+      title,
+      artist,
+      optimize: true,
+    };
+
     getSong(options).then((song) => {
       console.log("Song Lyrics:");
       console.log(song.lyrics);
     });
   });
-});
+
+program.addHelpCommand();
+
+program.parse(process.argv);
