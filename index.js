56621355d0fa94103a9aa73f6d879e23888efe1b
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2023-06-26 12:33:40 +0300

    feat : adding readline for user

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
+const readline = require("readline");
 const getLyrics = require("./library/getLyrics");
 const getSong = require("./library/getSong");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const options = {
   apiKey: "SZ2FBOqVDvzzk7OAVIzD06a9nDPlbEV6qsVIMzzqFUHS9klnUBzsK3dXUvoMTV2E",
-  title: "talking to the moon",
-  artist: "bruno mars",
+  title: "",
+  artist: "",
   optimize: true,
 };
 
-getLyrics(options).then((lyrics) => console.log(lyrics));
-getSong(options).then((song) => {
-  console.log(`${song.lyrics}`);
-});
+rl.question("Enter the title of the song: ", (title) => {
+  options.title = title;
 
+  rl.question("Enter the artist: ", (artist) => {
+    options.artist = artist;
+
+    rl.close();
+
+    // Fetch lyrics
+    getLyrics(options).then((lyrics) => {
+      console.log("Lyrics:");
+      console.log(lyrics);
+    });
+
+    // Fetch song
+    getSong(options).then((song) => {
+      console.log("Song Lyrics:");
+      console.log(song.lyrics);
+    });
+  });
+});
