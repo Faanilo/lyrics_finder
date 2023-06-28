0e3ece570973f89510fd4ffb1541b34ef58e09dd
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2023-06-28 21:04:23 +0300

    update version

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const getSong = require("./library/getSong");
 dotenv.config();
 
 const apiKey = process.env.API_KEY;
-const version = "1.0"; 
+const version = "1.0.2"; 
 
 program
   .command("lyrics <title> <artist>")
